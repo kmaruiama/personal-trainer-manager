044f68da31fbd86f14a830f68b50897243bf592e
kmaruiama <contatokmaruiama@gmail.com>
2024-12-30 20:03:06 -0300

    html dos programas feito. A inserção no backend precisa ser completamente refeita para permitir a adicao de novas séries e nao somente a edicao das que ja existem

--- src/app/customer/profile/customer-profile/blueprint/customer-blueprint/customer-blueprint.component.ts
+++ src/app/customer/profile/customer-profile/blueprint/customer-blueprint/customer-blueprint.component.ts
@@ -1,19 +1,21 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonContent, IonTitle, IonInput, IonItem, IonLabel, IonButton } from "@ionic/angular/standalone";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-blueprint',
   templateUrl: './customer-blueprint.component.html',
   styleUrls: ['./customer-blueprint.component.scss'],
   standalone: true,
-  imports: []
+  imports: [IonButton, IonLabel, IonTitle, IonContent, CommonModule]
 })
 export class CustomerBlueprintComponent implements OnInit {
   private customerId: number = 0;
   private customerName: string = "";
-  private nomeDoPrograma: string = "";
-  private workouts: Workout[] = [];
+  protected nomeDoPrograma: string = "";
+  protected workouts: Workout[] = [];
 
   constructor(private router: Router, private http: HttpClient) {}
 
